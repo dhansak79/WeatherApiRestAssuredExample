efd5a7ae1b0ab61065e264028cf6acc8d294559e
Daniel Webb <daniel.webb@me.com>
2022-05-30 15:56:23 +0100

    added ability to handle api key from .properties file

--- src/test/java/com/sparta/weather/ExampleTest.java
+++ src/test/java/com/sparta/weather/ExampleTest.java
@@ -1,12 +1,31 @@
 package com.sparta.weather;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ExampleTest {
 
+  private static String apiKey;
+
+  @BeforeAll
+  static void getProperties() {
+    try {
+      Properties properties = new Properties();
+      properties.load( new FileReader( "src/test/resources/weatherapi.properties" ) );
+      apiKey = properties.getProperty( "apikey" );
+    } catch ( IOException e ) {
+      throw new RuntimeException( e );
+    }
+  }
+
   @Test
   public void exampleTest() {
+    System.out.println( apiKey );
     Assertions.assertTrue( true );
   }
 
