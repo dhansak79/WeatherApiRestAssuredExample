c5699e9a0f44c66cb81b24ad34f6789d787ccdca
Daniel Webb <daniel.webb@me.com>
2022-05-31 15:31:20 +0100

    more trump

--- src/test/java/com/sparta/tronald/DonaldTrumpTest.java
+++ src/test/java/com/sparta/tronald/DonaldTrumpTest.java
@@ -4,14 +4,16 @@ import io.restassured.RestAssured;
 import io.restassured.response.ResponseBody;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
 
 public class DonaldTrumpTest {
 
   @Test
   public void whatDoYouThinkAbout() {
+    String query = "Obama";
     RestAssured.baseURI = "https://api.tronalddump.io";
-    ResponseBody responseBody = when().get( "/random/quote" ).getBody();
+    ResponseBody responseBody = given().queryParam( "query", query )
+            .when().get( "/search/quote/" ).getBody();
     System.out.println( responseBody.asPrettyString() );
   }
 
