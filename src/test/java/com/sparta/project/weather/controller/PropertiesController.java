49332d688216298c78237b0ce536b8be9c21f84b
Daniel Webb <daniel.webb@me.com>
2022-05-31 20:35:05 +0100

    adding some logging

--- src/test/java/com/sparta/project/weather/controller/PropertiesController.java
+++ src/test/java/com/sparta/project/weather/controller/PropertiesController.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesController {
-
+  
   public static String getApiKey() {
     try {
       Properties properties = new Properties();
       properties.load(new FileReader("src/test/resources/weatherapi.properties"));
       return properties.getProperty("apikey");
     } catch (IOException e) {
+      System.out.println("Getting property from system properties: " + System.getProperty( "weatherapikey" ));
       return System.getProperty( "weatherapikey" );
     }
   }
