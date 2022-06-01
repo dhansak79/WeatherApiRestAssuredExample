ea4b39befe00cb6c9c78305891a0375cf75a11a4
GH <georgeheath.98840@gmail.com>
2022-06-01 11:53:19 +0100

    GH Merge conflict

--- src/test/java/com/sparta/project/weather/AirPollutionTest.java
+++ src/test/java/com/sparta/project/weather/AirPollutionTest.java
@@ -1,25 +1,10 @@
 package com.sparta.project.weather;
 
 import com.sparta.project.weather.controller.PropertiesController;
-<<<<<<< HEAD
-import com.sparta.weather.model.AirPollution;
-import io.restassured.RestAssured;
-import io.restassured.response.ResponseBody;
-import org.hamcrest.object.HasEqualValues;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-=======
 import com.sparta.project.weather.model.airpollutionmodels.AirPollution;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.get;
->>>>>>> e86a3548a28386098104eb7264f0cd56f61eb44e
 
 public class AirPollutionTest {
 
